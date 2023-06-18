46d6ff1dc4c0d884f8d99853eed8e5d3337ea3c7
Hukasx0 <hubertkasp13@gmail.com>
2023-06-18 16:40:01 +0200

    Migrated to Hyper 1.0 library

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,13 @@ use std::time::SystemTime;
 use tokio::sync::Mutex;
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use hyper::{Body, Request, Response, Server};
-use hyper::service::{make_service_fn, service_fn};
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use tokio::net::TcpListener;
+
 
 fn sha2_hash(data: &str) -> [u8; 32] {
     let mut hasher = Sha256::new();
@@ -13,8 +18,8 @@ fn sha2_hash(data: &str) -> [u8; 32] {
     hasher.finalize().into()
 }
 
-async fn server_response(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("Hello world!".into()))
+async fn server_response(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new(Bytes::from("Hello world!"))))
 }
 
 /* Database :=
@@ -30,14 +35,19 @@ type Database = HashMap<String,HashMap<String,Option<SystemTime>>>;
 type AsyncDatabase = Mutex<Database>;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 5379));
-    let make_svc = make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(server_response))
-    });
-    let server = Server::bind(&addr).serve(make_svc);
+    let listener = TcpListener::bind(addr).await?;
     println!("Sesser works on http://127.0.0.1:5379");
-    if let Err(e) = server.await {
-        eprintln!("Server error: {}", e);
+    loop {
+        let (stream, _) = listener.accept().await?;
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(stream, service_fn(server_response))
+                .await
+            {
+                eprintln!("Error serving connection: {:?}", err);
+            }
+        });
     }
 }
